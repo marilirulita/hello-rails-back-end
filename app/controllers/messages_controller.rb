b00bdde20de76c85905dff0c7bc18d9a16df6e97
mar <marilirulita@gmail.com>
2022-09-29 14:56:09 -0700

    add and configure cors

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,6 @@ class MessagesController < ApplicationController
   def index
     @message = Message.find(rand(1..Message.count))
 
-    render json: @message
+    render json: { greet: @message.text }, status: :ok
   end
 end
